649aaa6a3991f29d62eb00f20cf28768ce56d1cc
AmandaChou <AmandaChou@itsower.com .tw>
2024-06-12 10:11:56 +0800

    net 6 api不行

--- Program.cs
+++ Program.cs
@@ -3,6 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.Configure<NuGet.Server.Core.Configuration.NuGetServerOptions>(options =>
+{
+    options.PackagePath = builder.Configuration["NuGet:PackagesPath"];
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,12 +20,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+
+app.UseStaticFiles();//±Ò¥ÎÀRºA
+app.UseDirectoryBrowser();//±Ò¥Î¥Ø¿ý
 
 app.Run();
